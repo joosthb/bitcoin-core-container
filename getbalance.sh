bd43e3a9c076a7963cef48e52892e08448835376
Joost Hendricksen <joost@hendricksen.net>
2025-06-05 08:10:20 +0200

    adding btcnode wallet watching scripts

--- getbalance.sh
+++ getbalance.sh
@@ -10,17 +10,6 @@ curl --user $btcuser:$btcpass \
        "params": []
      }' \
      -H 'content-type: text/plain;' \
-     $btcresource/wallet/Wallet2  | jq
-
-curl --user $btcuser:$btcpass \
-     --data-binary '{
-       "jsonrpc": "1.0",
-       "id": "curltest",
-       "method": "listdescriptors",
-       "params": []
-     }' \
-     -H 'content-type: text/plain;' \
-     $btcresource/wallet/Wallet2  | jq
-
+     $btcresource/wallet/cormorant  | jq
 
      
